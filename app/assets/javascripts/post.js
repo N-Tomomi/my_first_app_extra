ce49432c9c818282675983369edae4aacd1e92d5
N-Tomomi <d_o_yukijiro@hotmail.co.jp>
2020-05-21 00:32:03 +0900

    favicon_setteing

---
+++ app/assets/javascripts/post.js
@@ -0,0 +1,28 @@
+'use strict';
+
+{
+
+  function update() {
+    document.querySelector('h1').textContent = 'Changed!';
+
+    const leftImage = document.querySelectorAll('.content__left--image');
+
+    leftImage.forEach((p) => {
+      p.style.backgroundColor = 'lightyellow';
+    });
+  
+    leftImage.forEach((p, index) => {
+      p.textContent = `${index}`;
+    });
+  
+  }
+
+  setTimeout(update, 1000);
+
+  
+  // document.getElementById('target').addEventListener('click', () => {
+  //   document.getElementById('target').style.color = 'red';
+  // });
+
+  // ここまでJS実験用　index.html.haml #target
+}
